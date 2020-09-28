9f22f60eb311134ec6cbdce9dccb5b180beb1ab1
j-blalock <justin.m.blalock@gmail.com>
2020-09-28 02:08:02 -0500

    class work

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-console.log('hello world');
+class Tomagatchi {
+    constructor (name) {
+    this.name = "";
+    this.clock = 0,
+    this.hunger = 0;
+    this.sleepiness = 0;
+    this.boredom = 0;
+    this.age = 0;
+    }
+    startgame() {
+
+    }
+    play() {
+        console.log('feed');
+    }
+    lightsOut() {
+
+    }
+    feed() {
+
+    }
+    evolve() {
+
+    }
+    die() {
+        
+    }
+ 
+};
